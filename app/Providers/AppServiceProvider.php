67de9a1ff6a8f7681c2955c6e49c63fe904502a3
andreslqr <me@andreslopez.com.mx>
2025-10-12 16:04:33 -0600

    conf: added translations and money helpers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use Filament\Actions\Action;
+use Filament\Forms\Components\Field;
+use Filament\Infolists\Components\Entry;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Support\Facades\FilamentTimezone;
+use Filament\Tables\Columns\Column;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +25,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        FilamentTimezone::set('America/Mexico_City'); 
+
+        Field::configureUsing(function(Field $field): void {
+            $field->translateLabel();
+        });
+        Column::configureUsing(function(Column $column): void {
+            $column->translateLabel();
+        });
+        Entry::configureUsing(function(Column $column): void {
+            $column->translateLabel();
+        });
+        Action::configureUsing(function(Action $action): void {
+            $action->translateLabel();
+        });
     }
 }
